ebd8a600475e9499daf1649940324d0afaffa6a4
Diksha Sharma <36410355+dishu1722@users.noreply.github.com>
2025-08-11 18:59:11 -0400

    updated functions.php file

--- Projects/Buffet Hire/themes/functions.php
+++ Projects/Buffet Hire/themes/functions.php
@@ -22,3 +22,33 @@ function custom_cart_items_list() {
     return $output;
 }
 add_shortcode( 'cart_items_list', 'custom_cart_items_list' );
+
+//OR
+function custom_cart_items_list() {
+    // Only run on frontend and if WooCommerce cart is available
+    if ( is_admin() ) {
+        // Don't run shortcode in admin area (like page editor)
+        return '';
+    }
+
+    if ( ! class_exists( 'WooCommerce' ) ) {
+        return 'WooCommerce not active.';
+    }
+
+    if ( ! WC()->cart ) {
+        return 'Cart not initialized.';
+    }
+
+    if ( WC()->cart->is_empty() ) {
+        return 'No items in cart.';
+    }
+    
+    $items = WC()->cart->get_cart();
+    $output = '';
+    foreach ( $items as $item => $values ) {
+        $product = $values['data']->get_name();
+        $output .= $product . "\n";
+    }
+    return nl2br( $output ); // Convert new lines to <br> for HTML output
+}
+add_shortcode( 'cart_items_list', 'custom_cart_items_list' );
