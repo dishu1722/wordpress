94848f4bc78cbe14b584254299a33767b4c9ab42
Diksha Sharma <36410355+dishu1722@users.noreply.github.com>
2025-10-09 12:52:43 -0400

    Update scripts.js

--- Projects/Waldholz/scripts.js
+++ Projects/Waldholz/scripts.js
@@ -1,3 +1,14 @@
+// Sticky header
+jQuery(document).ready(function($) {
+    var header = $('header'); // Change this selector to match your header element
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 50) { // When scrolled more than 50px
+            header.addClass('scrolled');
+        } else {
+            header.removeClass('scrolled');
+        }
+    });
+});
 
 // Adding the tooltip and customizing the position for UK to make- england/Scotland in MAPGEO plugin.
 
